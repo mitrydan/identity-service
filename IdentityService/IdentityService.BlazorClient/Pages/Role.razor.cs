6cac386c45aa783b9d4e1f5127d9fc9eaebe10db
mitrydan <ddmitriy93@gmail.com>
2020-10-04 22:05:09 +0300

    Добавлен проект IdentityService.BlazorClient

---
+++ IdentityService/IdentityService.BlazorClient/Pages/Role.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace IdentityService.BlazorClient.Pages
+{
+    public partial class Role : ComponentBase
+    {
+        private string title = "Text";
+    }
+}
